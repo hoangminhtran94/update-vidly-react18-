15c99acbaa9f4c8cd37d4816c667977cafad56f2
minhhoangtran94 <95241885+hoangminhtran94@users.noreply.github.com>
2023-06-13 02:47:21 -0400

    update UI

--- src/components/ChatListItem/ChatListItem.tsx
+++ src/components/ChatListItem/ChatListItem.tsx
@@ -13,14 +13,16 @@ const ChatListItem: FC<{
 
   return (
     <div
-      className=" flex relative rounded-full hover:scale-105   shadow-md border border-indigo-700 items-center cursor-pointer overflow-hidden"
+      className=" flex relative rounded-full hover:scale-105   shadow-md border border-indigo-700 items-center cursor-pointer "
       onClick={onClick}
     >
       <Image
         src={chatList.user.image}
         className=" rounded-full me-2 w-[30px] h-[30px] object-cover"
       />
-      <p className="m-0 text-xs">{chatList.user.firstName}</p>
+      <p className="m-0 text-xs max-w-[50%] whitespace-nowrap overflow-hidden">
+        {chatList.user.firstName}
+      </p>
       {chatList.unreadCounter > 0 && (
         <span className="absolute w-5 h-5 rounded-full -top-2 -right-2 bg-red-500 shadow-md text-white text-center">
           {chatList.unreadCounter}
